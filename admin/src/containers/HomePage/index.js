487f20e0ac229ebef121ca3f92293bc24596671a
paulajbastos <paulajbastos@paulajbastoss-MacBook-Air-2.local>
2020-10-08 18:34:08 -0300

    customizando a home

--- admin/src/containers/HomePage/index.js
+++ admin/src/containers/HomePage/index.js
@@ -6,14 +6,12 @@
 /* eslint-disable */
 import React, { memo, useMemo } from 'react';
 import { FormattedMessage } from 'react-intl';
-import { get, upperFirst } from 'lodash';
-import { auth, LoadingIndicatorPage } from 'strapi-helper-plugin';
+import { LoadingIndicatorPage } from 'strapi-helper-plugin';
 import PageTitle from '../../components/PageTitle';
 import { useModels } from '../../hooks';
 
 import useFetch from './hooks';
 import { ALink, Block, Container, LinkWrapper, P, Wave, Separator } from './components';
-import BlogPost from './BlogPost';
 import SocialLink from './SocialLink';
 
 const FIRST_BLOCK_LINKS = [
@@ -33,73 +31,26 @@ const FIRST_BLOCK_LINKS = [
 const SOCIAL_LINKS = [
   {
     name: 'GitHub',
-    link: 'https://github.com/strapi/strapi/',
+    link: 'https://github.com/react-avancado',
   },
   {
     name: 'Slack',
-    link: 'https://slack.strapi.io/',
-  },
-  {
-    name: 'Medium',
-    link: 'https://medium.com/@strapi',
+    link: 'https://bit.ly/will-slack',
   },
   {
     name: 'Twitter',
-    link: 'https://twitter.com/strapijs',
-  },
-  {
-    name: 'Reddit',
-    link: 'https://www.reddit.com/r/Strapi/',
-  },
-  {
-    name: 'Forum',
-    link: 'https://forum.strapi.io',
+    link: 'https://twitter.com/Willian_Justen',
   },
 ];
 
 const HomePage = ({ global: { plugins }, history: { push } }) => {
   const { error, isLoading, posts } = useFetch();
   // Temporary until we develop the menu API
-  const { collectionTypes, singleTypes, isLoading: isLoadingForModels } = useModels();
-
-  const handleClick = e => {
-    e.preventDefault();
-
-    push(
-      '/plugins/content-type-builder/content-types/plugins::users-permissions.user?modalType=contentType&kind=collectionType&actionType=create&settingType=base&forTarget=contentType&headerId=content-type-builder.modalForm.contentType.header-create&header_icon_isCustom_1=false&header_icon_name_1=contentType&header_label_1=null'
-    );
-  };
-
-  const hasAlreadyCreatedContentTypes = useMemo(() => {
-    const filterContentTypes = contentTypes => contentTypes.filter(c => c.isDisplayed);
-
-    return (
-      filterContentTypes(collectionTypes).length > 1 || filterContentTypes(singleTypes).length > 0
-    );
-  }, [collectionTypes, singleTypes]);
+  const {isLoading: isLoadingForModels } = useModels();
 
   if (isLoadingForModels) {
     return <LoadingIndicatorPage />;
   }
-
-  const headerId = hasAlreadyCreatedContentTypes
-    ? 'HomePage.greetings'
-    : 'app.components.HomePage.welcome';
-  const username = get(auth.getUserInfo(), 'username', '');
-  const linkProps = hasAlreadyCreatedContentTypes
-    ? {
-        id: 'app.components.HomePage.button.blog',
-        href: 'https://strapi.io/blog/',
-        onClick: () => {},
-        type: 'blog',
-        target: '_blank',
-      }
-    : {
-        id: 'app.components.HomePage.create',
-        href: '',
-        onClick: handleClick,
-        type: 'documentation',
-      };
 
   return (
     <>
@@ -110,68 +61,9 @@ const HomePage = ({ global: { plugins }, history: { push } }) => {
         <div className="row">
           <div className="col-lg-8 col-md-12">
             <Block>
-              <Wave />
-              <FormattedMessage
-                id={headerId}
-                values={{
-                  name: upperFirst(username),
-                }}
-              >
-                {msg => <h2 id="mainHeader">{msg}</h2>}
-              </FormattedMessage>
-              {hasAlreadyCreatedContentTypes ? (
-                <FormattedMessage id="app.components.HomePage.welcomeBlock.content.again">
-                  {msg => <P>{msg}</P>}
-                </FormattedMessage>
-              ) : (
-                <FormattedMessage id="HomePage.welcome.congrats">
-                  {congrats => {
-                    return (
-                      <FormattedMessage id="HomePage.welcome.congrats.content">
-                        {content => {
-                          return (
-                            <FormattedMessage id="HomePage.welcome.congrats.content.bold">
-                              {boldContent => {
-                                return (
-                                  <P>
-                                    <b>{congrats}</b>&nbsp;
-                                    {content}&nbsp;
-                                    <b>{boldContent}</b>
-                                  </P>
-                                );
-                              }}
-                            </FormattedMessage>
-                          );
-                        }}
-                      </FormattedMessage>
-                    );
-                  }}
-                </FormattedMessage>
-              )}
-              {hasAlreadyCreatedContentTypes && (
-                <div style={{ marginTop: isLoading ? 60 : 50 }}>
-                  {posts.map((post, index) => (
-                    <BlogPost
-                      {...post}
-                      key={post.link}
-                      isFirst={index === 0}
-                      isLoading={isLoading}
-                      error={error}
-                    />
-                  ))}
-                </div>
-              )}
-              <FormattedMessage id={linkProps.id}>
-                {msg => (
-                  <ALink
-                    rel="noopener noreferrer"
-                    {...linkProps}
-                    style={{ verticalAlign: ' bottom', marginBottom: 5 }}
-                  >
-                    {msg}
-                  </ALink>
-                )}
-              </FormattedMessage>
+              <h2 id="mainHeader">Bem vindo ao Won Games!</h2>
+              <p>Ao lado você pode inserir diversos jogos, categorias e
+                publishers para a nossa maravilhosa loja de jogos!{" "}</p>
               <Separator style={{ marginTop: 37, marginBottom: 36 }} />
               <div style={{ display: 'flex', justifyContent: 'space-between' }}>
                 {FIRST_BLOCK_LINKS.map((data, index) => {
@@ -194,21 +86,15 @@ const HomePage = ({ global: { plugins }, history: { push } }) => {
 
           <div className="col-md-12 col-lg-4">
             <Block style={{ paddingRight: 30, paddingBottom: 0 }}>
-              <FormattedMessage id="HomePage.community">{msg => <h2>{msg}</h2>}</FormattedMessage>
-              <FormattedMessage id="app.components.HomePage.community.content">
-                {content => <P style={{ marginTop: 7, marginBottom: 0 }}>{content}</P>}
-              </FormattedMessage>
-              <FormattedMessage id="HomePage.roadmap">
-                {msg => (
-                  <ALink
-                    rel="noopener noreferrer"
-                    href="https://portal.productboard.com/strapi/1-public-roadmap/tabs/2-under-consideration"
-                    target="_blank"
-                  >
-                    {msg}
-                  </ALink>
-                )}
-              </FormattedMessage>
+              <h2 id="HomePage.community">Veja nossos links!</h2>
+              <p id="app.components.HomePage.community.content"style={{ marginTop: 7, marginBottom: 0 }}>Em caso de dúvidas ou sugestões, só ir em algum de nossos links =)</p>
+               <ALink
+                rel="noopener noreferrer"
+                href="https://reactavancado.com.br"
+                target="_blank"
+              >
+                Veja o nosso planejamento de módulos
+              </ALink>
 
               <Separator style={{ marginTop: 18 }} />
               <div
