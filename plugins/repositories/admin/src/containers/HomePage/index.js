d1262a8ae3d3d0d0196b5ce55db1e041d94eefe9
paulajbastos <paulajbastos@paulajbastoss-MacBook-Air-2.local>
2020-10-09 17:59:49 -0300

    instalando plugin externo

--- plugins/repositories/admin/src/containers/HomePage/index.js
+++ plugins/repositories/admin/src/containers/HomePage/index.js
@@ -1,19 +1,54 @@
-/*
- *
- * HomePage
- *
- */
+import React, { useState, useEffect, memo } from "react";
+import { Header } from "@buffetjs/custom";
+import { Table } from "@buffetjs/core";
+import axios from "axios";
 
-import React, { memo } from 'react';
-// import PropTypes from 'prop-types';
-import pluginId from '../../pluginId';
+
+import style from 'styled-components'
+
+
+const Wrapper = style.div`
+  padding: 18px 30px;
+  p {
+    margin-top: 1rem;
+  }
+`;
+
 
 const HomePage = () => {
+
+  const [rows, setRows] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://api.github.com/users/React-avancado/repos")
+      .then((res) => setRows(res.data))
+      .catch((e) => strapi.notification.error(`Ops...github API error, ${e}`));
+  }, []);
+
+  const headers = [
+  {
+    name: "Name",
+    value: "name",
+  },
+  {
+    name: "Description",
+    value: "description",
+  },
+  {
+    name: "Url",
+    value: "html_url",
+  },
+];
+
   return (
-    <div>
-      <h1>{pluginId}&apos;s HomePage</h1>
-      <p>Happy coding</p>
-    </div>
+    <Wrapper>
+      <Header
+        title={{ label: "React Avançado Repositories" }}
+        content="A list of our repositories in React Avançado course"
+      ></Header>
+      <Table headers={headers} rows={rows} />
+    </Wrapper>
   );
 };
 
