609117bd9e2f2f8de96116a309591afb4ff7dec2
paulajbastos <paulajbastos@paulajbastoss-MacBook-Air-2.local>
2020-10-27 18:30:02 -0300

    Criando dados relacionais de Many to

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -33,7 +33,7 @@ async function getGameInfo(slug) {
 }
 
 async function getByName(name, entityName) {
-  const item = await strapi.services[entityName].find({name});
+  const item = await strapi.services[entityName].find({ name });
   return item.length ? item[0] : null;
 }
 
@@ -43,28 +43,55 @@ async function create(name, entityName) {
   if (!item) {
     return await strapi.services[entityName].create({
       name,
-      slug: slugify(name, { lower: true }),
+      slug: slugify(name, { strict: true, lower: true }),
     });
   }
 }
 
+async function createManyToManyData(products) {
+  const developers = {};
+  const publishers = {};
+  const categories = {};
+  const platforms = {};
+
+  products.forEach((product) => {
+    const { developer, publisher, genres, supportedOperatingSystems } = product;
+
+    genres &&
+      genres.forEach((item) => {
+        categories[item] = true;
+      });
+    supportedOperatingSystems &&
+      supportedOperatingSystems.forEach((item) => {
+        platforms[item] = true;
+      });
+    developers[developer] = true;
+    publishers[publisher] = true;
+  });
+
+  return Promise.all([
+    ...Object.keys(developers).map((name) => create(name, "developer")),
+    ...Object.keys(publishers).map((name) => create(name, "publisher")),
+    ...Object.keys(categories).map((name) => create(name, "category")),
+    ...Object.keys(platforms).map((name) => create(name, "platform")),
+  ]);
+}
 
 module.exports = {
   populate: async (params) => {
     const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`;
+
     const {
       data: { products },
     } = await axios.get(gogApiUrl);
-    // console.log("Chamando serviço populate", products[1]);
 
-    await create(products[2].publisher, 'publisher');
-    await create(products[2].developer, 'developer');
+    // console.log(products[0]);
 
-    // await strapi.services.publisher.create({
-    //   name: products[1].publisher,
-    //   slug: slugify(products[1].publisher).toLowerCase(),
-    // });
+    await createManyToManyData([products[2], products[3]])
+
+    // await create(products[1].publisher, "publisher");
+    // await create(products[1].developer, "developer");
+
     // console.log(await getGameInfo(products[1].slug));
-    // console.log(await getByName(products[1].name, 'publisher'));
   },
 };
