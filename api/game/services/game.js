109ce7e71de3f187589998e97bd555757379dda2
paulajbastos <paulajbastos@paulajbastoss-MacBook-Air-2.local>
2020-10-27 17:46:48 -0300

    criando dados com service create

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/services.html#core-services)
@@ -6,16 +6,17 @@
  */
 
 const axios = require("axios");
+const slugify = require("slugify");
 
 async function getGameInfo(slug) {
   const jsdom = require("jsdom");
-  const {JSDOM} = jsdom;
+  const { JSDOM } = jsdom;
   const body = await axios.get(`https://www.gog.com/game/${slug}`);
   const dom = new JSDOM(body.data);
 
   const ratingElement = dom.window.document.querySelector(
     ".age-restrictions__icon use"
-  )
+  );
 
   const description = dom.window.document.querySelector(".description");
 
@@ -33,10 +34,16 @@ async function getGameInfo(slug) {
 
 module.exports = {
   populate: async (params) => {
-    const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`
-    const { data: { products } } = await axios.get(gogApiUrl)
-    console.log('Chamando serviço populate', products);
+    const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`;
+    const {
+      data: { products },
+    } = await axios.get(gogApiUrl);
+    console.log("Chamando serviço populate", products[1]);
 
-    console.log(await getGameInfo(products[1].slug));
-  }
+    await strapi.services.publisher.create({
+      name: products[1].publisher,
+      slug: slugify(products[1].publisher),
+    });
+    // console.log(await getGameInfo(products[1].slug));
+  },
 };
